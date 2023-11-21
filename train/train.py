4239c1a9e9960605cc4f0f92bddd304c98d64e7a
username0x0000 <ghost92747@icloud.com>
2023-11-22 03:46:18 +0900

    make loss, optim

--- train/train.py
+++ train/train.py
@@ -1,11 +1,22 @@
+import sys
+sys.path.append('./model')
+sys.path.append('.')
+sys.path.append('./data')
 import config as cfg
+import torch
+from torch import nn
+from torch import optim
+from backbone import *
+from head import *
+from get_model import *
+from get_dataloader import *
 
 
 def train_main():
-    dataloader = get_dataloader(cfg.DATA, 'train')
-    model = get_model(cfg.MODEL)
+    dataloader = get_dataloader(cfg=cfg)
+    model = get_model(cfg.MODEL, 'ResNet50')
     loss = get_loss(cfg.LOSS)
-    optimizer = get_optimizer(cfg.OPTIMIZER)
+    optimizer = get_optimizer(cfg.OPTIMIZER, model)
 
     trainer = Trainer(model, loss, optimizer, cfg.TRAIN)
     trainer.train(dataloader)
@@ -17,10 +28,14 @@ def train_main():
 
 def get_loss(cfg):
     if cfg['type'] == 'CrossEntropy':
-        loss = torch.cross_entropy()
+        loss = nn.CrossEntropyLoss()
 
     return loss
 
 
-def get_optimizer():
-    pass
+def get_optimizer(cfg, model):
+    optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
+
+
+if __name__ == '__main__':
+    train_main()
